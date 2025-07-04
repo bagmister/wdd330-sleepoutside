46c351b092e814282816fe172d8c1103631b247d
kevin bagley <bagmister@yahoo.com>
2025-07-04 09:42:26 -0600

    fix broken cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,12 @@
 import { setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+let cartCollection = []
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  cartCollection.push(product)
+  setLocalStorage("so-cart", cartCollection);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
